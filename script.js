dcf04919a4836bfb4259ad10bccc9e37a054d7cf
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-05 17:15:26 +0400

    fix some bugs

--- script.js
+++ script.js
@@ -14,10 +14,34 @@ accordion_header.forEach((element) => {
   element.addEventListener("click", function () {
     if (this.textContent.trim() == "Accordion Item #1") {
       txtOne.classList.toggle("none");
+      if (this.innerHTML.includes("down.svg")) {
+        this.innerHTML = `<button class="accordion-button" type="button">
+    Accordion Item #1
+  <img src="images/up.svg"></button>`;
+      } else
+        this.innerHTML = `<button class="accordion-button" type="button">
+    Accordion Item #1
+  <img src="images/down.svg"></button>`;
     } else if (this.textContent.trim() == "Accordion Item #2") {
       txtTwo.classList.toggle("none");
+      if (this.innerHTML.includes("down.svg")) {
+        this.innerHTML = `<button class="accordion-button" type="button">
+    Accordion Item #2
+  <img src="images/up.svg"></button>`;
+      } else
+        this.innerHTML = `<button class="accordion-button" type="button">
+    Accordion Item #2
+  <img src="images/down.svg"></button>`;
     } else if (this.textContent.trim() == "Accordion Item #3") {
       txtThree.classList.toggle("none");
+      if (this.innerHTML.includes("down.svg")) {
+        this.innerHTML = `<button class="accordion-button" type="button">
+    Accordion Item #3
+  <img src="images/up.svg"></button>`;
+      } else
+        this.innerHTML = `<button class="accordion-button" type="button">
+    Accordion Item #3
+  <img src="images/down.svg"></button>`;
     }
   });
 });
