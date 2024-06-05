4db846135abb2b93e7087950c84bf6284535aaf0
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-05 16:29:17 +0400

    done

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ const accordion_header = document.querySelectorAll(".accordion-header");
 const txtOne = document.getElementById("accardion_one");
 const txtTwo = document.getElementById("accardion_two");
 const txtThree = document.getElementById("accardion_three");
-const accordion_button = document.querySelectorAll("accordion-button");
-const img = document.createElement("img");
-
-img.setAttribute("src", "images/down.svg");
-accordion_button.forEach((element) => {
-  element.appendChild(img);
-});
 
 accordion_header.forEach((element) => {
   element.addEventListener("click", (e) => {
