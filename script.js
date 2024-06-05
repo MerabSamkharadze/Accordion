020129f2082e8c74e4a501e412273c82a10092c1
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-05 17:04:27 +0400

    add icones

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ const accordion_header = document.querySelectorAll(".accordion-header");
 const txtOne = document.getElementById("accardion_one");
 const txtTwo = document.getElementById("accardion_two");
 const txtThree = document.getElementById("accardion_three");
+const accordion_button = document.querySelectorAll(".accordion-button");
+
+accordion_button.forEach((element) => {
+  const icone = document.createElement("img");
+  icone.setAttribute("src", "images/down.svg");
+  element.appendChild(icone);
+});
 
 accordion_header.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    if (e.target.textContent.trim() == "Accordion Item #1") {
+  element.addEventListener("click", function () {
+    if (this.textContent.trim() == "Accordion Item #1") {
       txtOne.classList.toggle("none");
-    } else if (e.target.textContent.trim() == "Accordion Item #2") {
+    } else if (this.textContent.trim() == "Accordion Item #2") {
       txtTwo.classList.toggle("none");
-    } else if (e.target.textContent.trim() == "Accordion Item #3") {
+    } else if (this.textContent.trim() == "Accordion Item #3") {
       txtThree.classList.toggle("none");
     }
   });
